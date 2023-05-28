ad5cc2d3cc49a0a7e7d9d7ca03482390bb43bb7c
Théodore Prévot <prevottheodore@gmail.com>
2023-05-28 14:36:18 +0200

    Docs: Improving config documentation

--- engineioxide/src/layer.rs
+++ engineioxide/src/layer.rs
@@ -106,26 +106,74 @@ impl EngineIoConfigBuilder {
             config: EngineIoConfig::default(),
         }
     }
+    /// The path to listen for engine.io requests on.
+    /// Defaults to "/engine.io".
     pub fn req_path(mut self, req_path: String) -> Self {
         self.config.req_path = req_path;
         self
     }
+
+    /// The interval at which the server will send a ping packet to the client.
+    /// Defaults to 25 seconds.
     pub fn ping_interval(mut self, ping_interval: Duration) -> Self {
         self.config.ping_interval = ping_interval;
         self
     }
+
+    // The amount of time the server will wait for a ping response from the client before closing the connection.
+    /// Defaults to 20 seconds.
     pub fn ping_timeout(mut self, ping_timeout: Duration) -> Self {
         self.config.ping_timeout = ping_timeout;
         self
     }
+
+    /// The maximum number of packets that can be buffered per connection before being emitted to the client.
+    ///
+    /// If the buffer if full the `emit()` method will return an error
+    /// ```
+    /// use engineioxide::{
+    ///     layer::{EngineIoHandler, EngineIoLayer},
+    ///     socket::Socket,
+    /// };
+    /// use std::sync::Arc;
+    /// #[derive(Clone)]
+    /// struct MyHandler;
+    ///
+    /// #[engineioxide::async_trait]
+    /// impl EngineIoHandler for MyHandler {
+    ///
+    ///     type Data = ();
+    ///     fn on_connect(self: Arc<Self>, socket: &Socket<Self>) {
+    ///         println!("socket connect {}", socket.sid);
+    ///     }
+    ///     fn on_disconnect(self: Arc<Self>, socket: &Socket<Self>) {
+    ///         println!("socket disconnect {}", socket.sid);
+    ///     }
+    ///
+    ///     async fn on_message(self: Arc<Self>, msg: String, socket: &Socket<Self>) {
+    ///         println!("Ping pong message {:?}", msg);
+    ///         socket.emit(msg).unwrap();
+    ///     }
+    ///
+    ///     async fn on_binary(self: Arc<Self>, data: Vec<u8>, socket: &Socket<Self>) {
+    ///         println!("Ping pong binary message {:?}", data);
+    ///         socket.emit_binary(data).unwrap();
+    ///     }
+    /// }
+    /// ```
     pub fn max_buffer_size(mut self, max_buffer_size: usize) -> Self {
         self.config.max_buffer_size = max_buffer_size;
         self
     }
+
+    /// The maximum number of bytes that can be received per http request.
+    /// Defaults to 100kb.
     pub fn max_payload(mut self, max_payload: u64) -> Self {
         self.config.max_payload = max_payload;
         self
     }
+
+    /// Build the config
     pub fn build(self) -> EngineIoConfig {
         self.config
     }
