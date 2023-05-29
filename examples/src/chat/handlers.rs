9ffa6fd84349fc46e0ee92ce3732d3f7c85841cb
Théodore Prévot <prevottheodore@gmail.com>
2023-05-29 23:27:10 +0200

    docs(chat): adding socket nickname field in extensions

--- examples/src/chat/handlers.rs
+++ examples/src/chat/handlers.rs
@@ -1,17 +1,35 @@
 use std::sync::Arc;
 
+use serde::Deserialize;
 use socketioxide::{adapter::LocalAdapter, Socket};
 use tracing::info;
 
+#[derive(Deserialize, Clone, Debug)]
+struct Nickname(String);
+
+#[derive(Deserialize)]
+struct Auth {
+    pub nickname: Nickname,
+}
+
 pub async fn handler(socket: Arc<Socket<LocalAdapter>>) {
     info!("Socket connected on / with id: {}", socket.sid);
+    if let Ok(data) = socket.handshake.data::<Auth>() {
+        info!("Nickname: {:?}", data.nickname);
+        socket.extensions.insert(data.nickname);
+    } else {
+        info!("No nickname provided, disconnecting...");
+        socket.disconnect().ok();
+        return;
+    }
 
     socket.on("message", |socket, message: String, _, _| async move {
-        info!("Message received: {}", message);
+        let Nickname(ref nickname) = *socket.extensions.get().unwrap();
+        info!("Message received from {}: {}", nickname, message);
     });
 
-    socket.on("nickname", |socket, nickname: String, _, _| async move {
-        info!("Nickname received: {}", nickname);
+    socket.on("nickname", |socket, nickname: Nickname, _, _| async move {
+        let previous = socket.extensions.insert(nickname.clone());
+        info!("Nickname changed from {:?} to {:?}", previous, nickname);
     });
-
 }
