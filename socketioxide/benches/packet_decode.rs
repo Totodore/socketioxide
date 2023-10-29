2acd187c15c6fd0ad5b65172bb81d347b99165d4
Totodore <prevottheodore@gmail.com>
2023-10-29 19:59:43 +0100

    bench(socketio): fix bench to work with new packet impl

--- socketioxide/benches/packet_decode.rs
+++ socketioxide/benches/packet_decode.rs
@@ -3,18 +3,15 @@ use engineioxide::sid::Sid;
 use socketioxide::{Packet, PacketData, ProtocolVersion};
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet connect on /", |b| {
-        let packet: String = Packet::connect(
-            black_box("/").to_string(),
-            black_box(Sid::ZERO),
-            ProtocolVersion::V5,
-        )
-        .try_into()
-        .unwrap();
+        let packet: String =
+            Packet::connect(black_box("/"), black_box(Sid::ZERO), ProtocolVersion::V5)
+                .try_into()
+                .unwrap();
         b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
     c.bench_function("Decode packet connect on /custom_nsp", |b| {
         let packet: String = Packet::connect(
-            black_box("/custom_nsp").to_string(),
+            black_box("/custom_nsp"),
             black_box(Sid::ZERO),
             ProtocolVersion::V5,
         )
@@ -27,21 +24,18 @@ fn criterion_benchmark(c: &mut Criterion) {
     const BINARY: [u8; 10] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     c.bench_function("Decode packet event on /", |b| {
         let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::event(
-            black_box("/").to_string(),
-            black_box("event").to_string(),
-            black_box(data.clone()),
-        )
-        .try_into()
-        .unwrap();
+        let packet: String =
+            Packet::event(black_box("/"), black_box("event"), black_box(data.clone()))
+                .try_into()
+                .unwrap();
         b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     c.bench_function("Decode packet event on /custom_nsp", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::event(
-            black_box("custom_nsp").to_string(),
-            black_box("event").to_string(),
+            black_box("custom_nsp"),
+            black_box("event"),
             black_box(data.clone()),
         )
         .try_into()
@@ -51,11 +45,8 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("Decode packet event with ack on /", |b| {
         let data = serde_json::to_value(DATA).unwrap();
-        let packet: Packet = Packet::event(
-            black_box("/").to_string(),
-            black_box("event").to_string(),
-            black_box(data.clone()),
-        );
+        let packet: Packet =
+            Packet::event(black_box("/"), black_box("event"), black_box(data.clone()));
         match packet.inner {
             PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
             _ => panic!("Wrong packet type"),
@@ -67,8 +58,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet event with ack on /custom_nsp", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet = Packet::event(
-            black_box("/custom_nsp").to_string(),
-            black_box("event").to_string(),
+            black_box("/custom_nsp"),
+            black_box("event"),
             black_box(data.clone()),
         );
         match packet.inner {
@@ -82,20 +73,16 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("Decode packet ack on /", |b| {
         let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::ack(
-            black_box("/").to_string(),
-            black_box(data.clone()),
-            black_box(0),
-        )
-        .try_into()
-        .unwrap();
+        let packet: String = Packet::ack(black_box("/"), black_box(data.clone()), black_box(0))
+            .try_into()
+            .unwrap();
         b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     c.bench_function("Decode packet ack on /custom_nsp", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::ack(
-            black_box("/custom_nsp").to_string(),
+            black_box("/custom_nsp"),
             black_box(data.clone()),
             black_box(0),
         )
@@ -107,8 +94,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet binary event (b64) on /", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::bin_event(
-            black_box("/").to_string(),
-            black_box("event").to_string(),
+            black_box("/"),
+            black_box("event"),
             black_box(data.clone()),
             black_box(vec![BINARY.to_vec().clone()]),
         )
@@ -120,8 +107,8 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet binary event (b64) on /custom_nsp", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::bin_event(
-            black_box("/custom_nsp").to_string(),
-            black_box("event").to_string(),
+            black_box("/custom_nsp"),
+            black_box("event"),
             black_box(data.clone()),
             black_box(vec![BINARY.to_vec().clone()]),
         )
@@ -133,7 +120,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet binary ack (b64) on /", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::bin_ack(
-            black_box("/").to_string(),
+            black_box("/"),
             black_box(data.clone()),
             black_box(vec![BINARY.to_vec().clone()]),
             black_box(0),
@@ -146,7 +133,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Decode packet binary ack (b64) on /custom_nsp", |b| {
         let data = serde_json::to_value(DATA).unwrap();
         let packet: String = Packet::bin_ack(
-            black_box("/custom_nsp").to_string(),
+            black_box("/custom_nsp"),
             black_box(data.clone()),
             black_box(vec![BINARY.to_vec().clone()]),
             black_box(0),
