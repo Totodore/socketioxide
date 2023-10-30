ab4edc48f8365d036556db0eaf3432429e2c1f18
Totodore <prevottheodore@gmail.com>
2023-10-30 02:03:19 +0100

    doc(examples): add a hyper_v1_service

---
+++ examples/hyper-v1-echo/hyper_v1_echo.rs
@@ -0,0 +1,75 @@
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+
+use engineioxide::{
+    config::EngineIoConfig,
+    handler::EngineIoHandler,
+    service::EngineIoService,
+    socket::{DisconnectReason, Socket},
+};
+use hyper_util::rt::TokioIo;
+use hyper_v1::server::conn::http1;
+use tokio::net::TcpListener;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
+
+#[derive(Debug, Clone)]
+struct MyHandler;
+
+#[engineioxide::async_trait]
+impl EngineIoHandler for MyHandler {
+    type Data = ();
+
+    fn on_connect(&self, socket: Arc<Socket<Self::Data>>) {
+        println!("socket connect {}", socket.id);
+    }
+    fn on_disconnect(&self, socket: Arc<Socket<Self::Data>>, reason: DisconnectReason) {
+        println!("socket disconnect {}: {:?}", socket.id, reason);
+    }
+
+    fn on_message(&self, msg: String, socket: Arc<Socket<Self::Data>>) {
+        println!("Ping pong message {:?}", msg);
+        socket.emit(msg).ok();
+    }
+
+    fn on_binary(&self, data: Vec<u8>, socket: Arc<Socket<Self::Data>>) {
+        println!("Ping pong binary message {:?}", data);
+        socket.emit_binary(data).ok();
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = FmtSubscriber::builder()
+        .with_line_number(true)
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let config = EngineIoConfig::builder()
+        .ping_interval(Duration::from_millis(300))
+        .ping_timeout(Duration::from_millis(200))
+        .max_payload(1e6 as u64)
+        .build();
+
+    let svc = EngineIoService::with_config(MyHandler, config);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+    // We start a loop to continuously accept incoming connections
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        // Use an adapter to access something implementing `tokio::io` traits as if they implement
+        // `hyper::rt` IO traits.
+        let io = TokioIo::new(stream);
+        let svc = svc.clone();
+
+        // Spawn a tokio task to serve multiple connections concurrently
+        tokio::task::spawn(async move {
+            // Finally, we bind the incoming connection to our `hello` service
+            if let Err(err) = http1::Builder::new().serve_connection(io, svc).await {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
