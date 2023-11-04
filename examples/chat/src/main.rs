ef9a21987d0c048b74fc12effec741ae67c28de3
Totodore <prevottheodore@gmail.com>
2023-11-04 17:39:22 +0100

    doc(example): use official chat example

--- examples/chat/src/main.rs
+++ examples/chat/src/main.rs
@@ -1,65 +1,113 @@
-mod handlers;
+use std::sync::atomic::AtomicUsize;
 
-use axum::response::{Html, IntoResponse};
-use axum::routing::get;
 use axum::Server;
-use futures::FutureExt;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use socketioxide::SocketIo;
-use tokio::signal;
 use tower::ServiceBuilder;
-use tower_http::cors::CorsLayer;
-use tracing::{error, info, Level};
+use tower_http::{cors::CorsLayer, services::ServeDir};
+use tracing::{error, info};
 use tracing_subscriber::FmtSubscriber;
 
-async fn serve_html() -> impl IntoResponse {
-    Html(include_str!("chat.html"))
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(transparent)]
+struct Username(String);
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+#[serde(rename_all = "camelCase", untagged)]
+enum Res {
+    Login {
+        #[serde(rename = "numUsers")]
+        num_users: usize,
+    },
+    UserEvent {
+        #[serde(rename = "numUsers")]
+        num_users: usize,
+        username: Username,
+    },
+    Message {
+        username: Username,
+        message: String,
+    },
+    Username {
+        username: Username,
+    },
 }
 
-fn set_shutdown_handler(io: SocketIo) -> tokio::sync::oneshot::Receiver<()> {
-    let (close_tx, close_rx) = tokio::sync::oneshot::channel::<()>();
-    tokio::spawn(async move {
-        match signal::ctrl_c().await {
-            Ok(()) => {}
-            Err(err) => {
-                error!("Unable to listen for shutdown signal: {}", err);
-            }
-        };
+static NUM_USERS: AtomicUsize = AtomicUsize::new(0);
 
-        io.close().await;
-        close_tx.send(()).unwrap();
-    });
-    close_rx
-}
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let subscriber = FmtSubscriber::builder()
-        .with_line_number(true)
-        .with_max_level(Level::DEBUG)
-        .finish();
+    let subscriber = FmtSubscriber::new();
 
     tracing::subscriber::set_global_default(subscriber)?;
 
     info!("Starting server");
 
     let (layer, io) = SocketIo::new_layer();
-    io.ns("/", handlers::handler);
 
-    // The shutdown handler is used to gracefully shutdown the server when a SIGINT or SIGTERM is emitted
-    // The socket.io will then gracefully close all connections
-    let on_shutdown = set_shutdown_handler(io.clone());
+    io.ns("/", move |s, _: Value| async move {
+        s.on("new message", |s, msg: String, _, _| async move {
+            let username = s.extensions.get::<Username>().unwrap().clone();
+            let msg = Res::Message {
+                username,
+                message: msg,
+            };
+            s.broadcast().emit("new message", msg).ok();
+        });
+
+        s.on("add user", move |s, username: String, _, _| async move {
+            if s.extensions.get::<Username>().is_some() {
+                return;
+            }
+            let i = NUM_USERS.fetch_add(1, std::sync::atomic::Ordering::SeqCst) + 1;
+            s.extensions.insert(Username(username.clone()));
+            s.emit("login", Res::Login { num_users: i }).ok();
+
+            let res = Res::UserEvent {
+                num_users: i,
+                username: Username(username),
+            };
+            s.broadcast().emit("user joined", res).ok();
+        });
+
+        s.on("typing", |s, _: [(); 0], _, _| async move {
+            let username = s.extensions.get::<Username>().unwrap().clone();
+            s.broadcast()
+                .emit("typing", Res::Username { username })
+                .ok();
+        });
+
+        s.on("stop typing", |s, _: [(); 0], _, _| async move {
+            let username = s.extensions.get::<Username>().unwrap().clone();
+            s.broadcast()
+                .emit("stop typing", Res::Username { username })
+                .ok();
+        });
+
+        s.on_disconnect(move |s, _| async move {
+            if let Some(username) = s.extensions.get::<Username>() {
+                let i = NUM_USERS.fetch_sub(1, std::sync::atomic::Ordering::SeqCst) - 1;
+                let res = Res::UserEvent {
+                    num_users: i,
+                    username: username.clone(),
+                };
+                s.broadcast().emit("user left", res).ok();
+            }
+        });
+    });
 
     let app = axum::Router::new()
-        .route("/", get(serve_html))
+        .nest_service("/", ServeDir::new("dist"))
         .layer(
             ServiceBuilder::new()
                 .layer(CorsLayer::permissive()) // Enable CORS policy
                 .layer(layer),
-        )
-        .with_state(io);
+        );
 
     let server = Server::bind(&"0.0.0.0:3000".parse().unwrap()).serve(app.into_make_service());
 
-    let server = server.with_graceful_shutdown(on_shutdown.map(drop));
     if let Err(e) = server.await {
         error!("server error: {}", e);
     }
