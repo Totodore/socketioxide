202f732a70612d30832b2e78c033127ea84c1ef4
Tausif <littletasteofheaven@protonmail.com>
2023-12-18 22:54:58 +0600

    Use &str and to_owned() to remove clippy warning

--- examples/react-rooms-chat/src/state.rs
+++ examples/react-rooms-chat/src/state.rs
@@ -16,14 +16,14 @@ pub struct MessageStore {
 }
 
 impl MessageStore {
-    pub async fn insert(&self, room: &String, message: Message) {
+    pub async fn insert(&self, room: &str, message: Message) {
         let mut binding = self.messages.write().await;
-        let messages = binding.entry(room.clone()).or_default();
+        let messages = binding.entry(room.to_owned()).or_default();
         messages.push_front(message);
         messages.truncate(20);
     }
 
-    pub async fn get(&self, room: &String) -> Vec<Message> {
+    pub async fn get(&self, room: &str) -> Vec<Message> {
         let messages = self.messages.read().await.get(room).cloned();
         messages.unwrap_or_default().into_iter().rev().collect()
     }
