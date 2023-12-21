b4754f64e6f236b90ba9fdae17ea2feafd34210e
Théodore Prévot <prevottheodore@gmail.com>
2023-12-21 20:46:21 +0000

    test(socketio/ack): fix ack tests

--- socketioxide/tests/acknowledgement.rs
+++ socketioxide/tests/acknowledgement.rs
@@ -1,19 +1,23 @@
 mod fixture;
 use fixture::{create_server, create_ws_connection};
+use futures::SinkExt;
 use socketioxide::extract::SocketRef;
 use tokio::sync::mpsc;
+use tokio_tungstenite::tungstenite::Message;
 
 #[tokio::test]
-pub async fn data_extractor() {
+pub async fn ack_emit_single_with_ack() {
     let io = create_server(2001).await;
-    let (tx, mut rx) = mpsc::channel::<i32>(4);
+    let (tx, mut rx) = mpsc::channel::<String>(4);
 
     io.ns("/", move |socket: SocketRef| async move {
         println!("Socket connected on / namespace with id: {}", socket.id);
         let ack = socket
             .emit_with_ack::<String>("test", "test")
+            .unwrap()
             .await
             .unwrap();
+        tx.try_send(ack.data).unwrap();
     });
 
     let mut stream = create_ws_connection(2001).await;
@@ -29,7 +33,8 @@ pub async fn data_extractor() {
         .send(Message::Text("42[\"ko_test\", 2]".to_string()))
         .await
         .unwrap();
-    assert_eq!(rx.recv().await.unwrap(), 1);
-    assert_eq!(rx.recv().await.unwrap(), 2);
+
+    assert_eq!(rx.recv().await.unwrap(), "test");
+    assert_eq!(rx.recv().await.unwrap(), "test");
     stream.close(None).await.unwrap();
 }
