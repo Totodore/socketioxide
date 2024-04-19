46c7a1b20803bff2ac49d17426eac3f378662d20
totodore <prevottheodore@gmail.com>
2024-04-19 19:17:43 -0300

    chore(bench): add bencher ci

---
+++ socketioxide/benches/extensions.rs
@@ -0,0 +1,30 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use socketioxide::extensions::Extensions;
+
+fn bench_extensions(c: &mut Criterion) {
+    let mut group = c.benchmark_group("extensions");
+    group.bench_function("concurrent_inserts", |b| {
+        let ext = Extensions::new();
+        b.iter(|| {
+            ext.insert(5i32);
+        });
+    });
+    // group.bench_function("concurrent_get", |b| {
+    //     b.iter(|| {
+    //         let mut ext = Extensions::new();
+    //         ext.insert(5i32);
+    //         ext.clear();
+    //     })
+    // });
+    // group.bench_function("concurrent_get_inserts", |b| {
+    //     b.iter(|| {
+    //         let mut ext = Extensions::new();
+    //         ext.insert(5i32);
+    //         ext.clear();
+    //     })
+    // });
+    group.finish();
+}
+
+criterion_group!(benches, bench_extensions);
+criterion_main!(benches);
