9553267c9afe86930ef92c098bf8bc0c232beed6
totodore <prevottheodore@gmail.com>
2024-04-19 17:32:46 -0300

    feat(socketio/extensions): use `RwLock<HashMap>` rather than `DashMap`

--- socketioxide/src/extensions.rs
+++ socketioxide/src/extensions.rs
@@ -2,13 +2,14 @@
 //!
 //! It is heavily inspired by the [`http::Extensions`] type from the `http` crate.
 //!
-//! The main difference is that it uses a [`DashMap`] instead of a [`HashMap`](std::collections::HashMap) to allow concurrent access.
+//! The main difference is that the inner [`HashMap`](std::collections::HashMap) is wrapped with an [`RwLock`]
+//! to allow concurrent access. Moreover, any value extracted from the map is cloned before being returned.
 //!
 //! This is necessary because [`Extensions`] are shared between all the threads that handle the same socket.
 
-use dashmap::DashMap;
+use std::collections::HashMap;
 use std::fmt;
-use std::ops::{Deref, DerefMut};
+use std::sync::RwLock;
 use std::{
     any::{Any, TypeId},
     hash::{BuildHasherDefault, Hasher},
@@ -17,78 +18,8 @@ use std::{
 /// TypeMap value
 type AnyVal = Box<dyn Any + Send + Sync>;
 
-/// The `AnyDashMap` is a `DashMap` that uses `TypeId` as keys and `Any` as values.
-type AnyDashMap = DashMap<TypeId, AnyVal, BuildHasherDefault<IdHasher>>;
-
-/// A wrapper for a `MappedRef` that implements `Deref` and `DerefMut` to allow
-/// easy access to the value.
-pub struct Ref<'a, T>(
-    dashmap::mapref::one::MappedRef<'a, TypeId, AnyVal, T, BuildHasherDefault<IdHasher>>,
-);
-
-impl<'a, T> Ref<'a, T> {
-    /// Get a reference to the value.
-    pub fn value(&self) -> &T {
-        self.0.value()
-    }
-}
-impl<'a, T> Deref for Ref<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.value()
-    }
-}
-
-/// A wrapper for a `MappedRefMut` that implements `Deref` and `DerefMut` to allow
-/// easy access to the value.
-pub struct RefMut<'a, T>(
-    dashmap::mapref::one::MappedRefMut<'a, TypeId, AnyVal, T, BuildHasherDefault<IdHasher>>,
-);
-
-impl<'a, T> RefMut<'a, T> {
-    /// Get a reference to the value.
-    pub fn value(&self) -> &T {
-        self.0.value()
-    }
-    /// Get a mutable reference to the value.
-    pub fn value_mut(&mut self) -> &mut T {
-        self.0.value_mut()
-    }
-}
-impl<'a, T> Deref for RefMut<'a, T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        self.value()
-    }
-}
-impl<'a, T> DerefMut for RefMut<'a, T> {
-    fn deref_mut(&mut self) -> &mut T {
-        self.value_mut()
-    }
-}
-
-impl<'a, T: fmt::Debug> fmt::Debug for Ref<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.value().fmt(f)
-    }
-}
-impl<'a, T: fmt::Debug> fmt::Debug for RefMut<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.value().fmt(f)
-    }
-}
-impl<'a, T: fmt::Display> fmt::Display for Ref<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.value().fmt(f)
-    }
-}
-impl<'a, T: fmt::Display> fmt::Display for RefMut<'a, T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.value().fmt(f)
-    }
-}
+/// The `AnyDashMap` is a `HashMap` that uses `TypeId` as keys and `Any` as values.
+type AnyHashMap = RwLock<HashMap<TypeId, AnyVal, BuildHasherDefault<IdHasher>>>;
 
 // With TypeIds as keys, there's no need to hash them. They are already hashes
 // themselves, coming from the compiler. The IdHasher just holds the u64 of
@@ -116,14 +47,13 @@ impl Hasher for IdHasher {
 ///
 /// It is heavily inspired by the `Extensions` type from the `http` crate.
 ///
-/// The main difference is that it uses a `DashMap` instead of a `HashMap` to allow concurrent access.
+/// The main difference is that the inner Map is wrapped with an `RwLock` to allow concurrent access.
 ///
 /// This is necessary because `Extensions` are shared between all the threads that handle the same socket.
 #[derive(Default)]
 pub struct Extensions {
-    /// The underlying map. It is not wrapped with an option because it would require insert calls to take a mutable reference.
-    /// Therefore an anydashmap will be allocated for every socket, even if it is not used.
-    map: AnyDashMap,
+    /// The underlying map
+    map: AnyHashMap,
 }
 
 impl Extensions {
@@ -131,11 +61,13 @@ impl Extensions {
     #[inline]
     pub fn new() -> Extensions {
         Extensions {
-            map: AnyDashMap::default(),
+            map: AnyHashMap::default(),
         }
     }
 
     /// Insert a type into this `Extensions`.
+    ///
+    /// The type must be cloneable and thread safe to be stored.
     ///
     /// If a extension of this type already existed, it will
     /// be returned.
@@ -149,18 +81,15 @@ impl Extensions {
     /// assert!(ext.insert(4u8).is_none());
     /// assert_eq!(ext.insert(9i32), Some(5i32));
     /// ```
-    pub fn insert<T: Send + Sync + 'static>(&self, val: T) -> Option<T> {
+    pub fn insert<T: Send + Sync + Clone + 'static>(&self, val: T) -> Option<T> {
         self.map
+            .write()
+            .unwrap()
             .insert(TypeId::of::<T>(), Box::new(val))
-            .and_then(|boxed| {
-                (boxed as Box<dyn Any + 'static>)
-                    .downcast()
-                    .ok()
-                    .map(|boxed| *boxed)
-            })
+            .and_then(|v| v.downcast().ok().map(|boxed| *boxed))
     }
 
-    /// Get a reference to a type previously inserted on this `Extensions`.
+    /// Get a cloned value of a type previously inserted on this `Extensions`.
     ///
     /// # Example
     ///
@@ -170,32 +99,15 @@ impl Extensions {
     /// assert!(ext.get::<i32>().is_none());
     /// ext.insert(5i32);
     ///
-    /// assert_eq!(*ext.get::<i32>().unwrap(), 5i32);
+    /// assert_eq!(ext.get::<i32>().unwrap(), 5i32);
     /// ```
-    pub fn get<T: Send + Sync + 'static>(&self) -> Option<Ref<'_, T>> {
+    pub fn get<T: Send + Sync + Clone + 'static>(&self) -> Option<T> {
         self.map
+            .read()
+            .unwrap()
             .get(&TypeId::of::<T>())
-            .and_then(|entry| entry.try_map(|r| r.downcast_ref::<T>()).ok())
-            .map(|r| Ref(r))
-    }
-
-    /// Get a mutable reference to a type previously inserted on this `Extensions`.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # use socketioxide::extensions::Extensions;
-    /// let ext = Extensions::new();
-    /// ext.insert(String::from("Hello"));
-    /// ext.get_mut::<String>().unwrap().push_str(" World");
-    ///
-    /// assert_eq!(*ext.get::<String>().unwrap(), "Hello World");
-    /// ```
-    pub fn get_mut<T: Send + Sync + 'static>(&self) -> Option<RefMut<'_, T>> {
-        self.map
-            .get_mut(&TypeId::of::<T>())
-            .and_then(|entry| entry.try_map(|r| r.downcast_mut::<T>()).ok())
-            .map(|r| RefMut(r))
+            .and_then(|v| v.downcast_ref::<T>())
+            .cloned()
     }
 
     /// Remove a type from this `Extensions`.
@@ -212,12 +124,11 @@ impl Extensions {
     /// assert!(ext.get::<i32>().is_none());
     /// ```
     pub fn remove<T: Send + Sync + 'static>(&self) -> Option<T> {
-        self.map.remove(&TypeId::of::<T>()).and_then(|(_, boxed)| {
-            (boxed as Box<dyn Any + 'static>)
-                .downcast()
-                .ok()
-                .map(|boxed| *boxed)
-        })
+        self.map
+            .write()
+            .unwrap()
+            .remove(&TypeId::of::<T>())
+            .and_then(|v| v.downcast().ok().map(|boxed| *boxed))
     }
 
     /// Clear the `Extensions` of all inserted extensions.
@@ -234,7 +145,7 @@ impl Extensions {
     /// ```
     #[inline]
     pub fn clear(&self) {
-        self.map.clear();
+        self.map.write().unwrap().clear();
     }
 
     /// Check whether the extension set is empty or not.
@@ -250,7 +161,7 @@ impl Extensions {
     /// ```
     #[inline]
     pub fn is_empty(&self) -> bool {
-        self.map.is_empty()
+        self.map.read().unwrap().is_empty()
     }
 
     /// Get the number of extensions available.
@@ -266,7 +177,7 @@ impl Extensions {
     /// ```
     #[inline]
     pub fn len(&self) -> usize {
-        self.map.len()
+        self.map.read().unwrap().len()
     }
 }
 
@@ -278,20 +189,26 @@ impl fmt::Debug for Extensions {
 
 #[test]
 fn test_extensions() {
-    #[derive(Debug, PartialEq)]
+    use std::sync::Arc;
+    #[derive(Debug, Clone, PartialEq)]
     struct MyType(i32);
+
+    #[derive(Debug, PartialEq)]
+    struct ComplexSharedType(u64);
+    let shared = Arc::new(ComplexSharedType(20));
 
     let extensions = Extensions::new();
 
     extensions.insert(5i32);
     extensions.insert(MyType(10));
+    extensions.insert(shared.clone());
 
-    assert_eq!(extensions.get().as_deref(), Some(&5i32));
-    assert_eq!(extensions.get_mut().as_deref_mut(), Some(&mut 5i32));
+    assert_eq!(extensions.get(), Some(5i32));
+    assert_eq!(extensions.get::<Arc<ComplexSharedType>>(), Some(shared));
 
     assert_eq!(extensions.remove::<i32>(), Some(5i32));
     assert!(extensions.get::<i32>().is_none());
 
     assert!(extensions.get::<bool>().is_none());
-    assert_eq!(extensions.get().as_deref(), Some(&MyType(10)));
+    assert_eq!(extensions.get(), Some(MyType(10)));
 }
