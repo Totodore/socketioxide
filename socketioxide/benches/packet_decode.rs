f0afe7d6fe2af790396d5d34cfe29a46db5ddf20
theodore <prevottheodore@gmail.com>
2024-08-10 08:19:10 +0200

    chore(socketio/bench): improve packet benchmarks

--- socketioxide/benches/packet_decode.rs
+++ socketioxide/benches/packet_decode.rs
@@ -1,139 +1,175 @@
 use bytes::Bytes;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use engineioxide::sid::Sid;
+use serde_json::to_value;
 use socketioxide::{
     packet::{Packet, PacketData},
+    parser::{CommonParser, Parse, TransportPayload},
     ProtocolVersion,
 };
+
+fn encode(packet: Packet<'_>) -> String {
+    match CommonParser::default().encode(black_box(packet)).0 {
+        TransportPayload::Str(d) => d.into(),
+        TransportPayload::Bytes(_) => panic!("testing only returns str"),
+    }
+}
+fn decode(value: String) -> Option<Packet<'static>> {
+    CommonParser::default()
+        .decode_str(black_box(value.into()))
+        .ok()
+}
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("socketio_packet/decode");
     group.bench_function("Decode packet connect on /", |b| {
-        let packet: String =
-            Packet::connect(black_box("/"), black_box(Sid::ZERO), ProtocolVersion::V5).into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
+        b.iter_batched(
+            || encode(Packet::connect("/", Sid::ZERO, ProtocolVersion::V5)),
+            decode,
+            BatchSize::SmallInput,
+        )
     });
     group.bench_function("Decode packet connect on /custom_nsp", |b| {
-        let packet: String = Packet::connect(
-            black_box("/custom_nsp"),
-            black_box(Sid::ZERO),
-            ProtocolVersion::V5,
+        b.iter_batched(
+            || {
+                encode(Packet::connect(
+                    "/custom_nsp",
+                    Sid::ZERO,
+                    ProtocolVersion::V5,
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     const DATA: &str = r#"{"_placeholder":true,"num":0}"#;
     const BINARY: Bytes = Bytes::from_static(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     group.bench_function("Decode packet event on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String =
-            Packet::event(black_box("/"), black_box("event"), black_box(data.clone())).into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
+        b.iter_batched(
+            || encode(Packet::event("/", "event", to_value(DATA).unwrap())),
+            decode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Decode packet event on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::event(
-            black_box("custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
+        b.iter_batched(
+            || {
+                encode(Packet::event(
+                    "custom_nsp",
+                    "event",
+                    to_value(DATA).unwrap(),
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.bench_function("Decode packet event with ack on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: Packet =
-            Packet::event(black_box("/"), black_box("event"), black_box(data.clone()));
-        match packet.inner {
-            PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
-            _ => panic!("Wrong packet type"),
-        };
-        let packet: String = packet.into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
+        b.iter_batched(
+            || {
+                let packet = Packet::event("/", "event", to_value(DATA).unwrap());
+                match packet.inner {
+                    PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
+                    _ => panic!("Wrong packet type"),
+                };
+                encode(packet)
+            },
+            decode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Decode packet event with ack on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::event(
-            black_box("/custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
-        );
-        match packet.inner {
-            PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
-            _ => panic!("Wrong packet type"),
-        };
-        let packet: String = packet.into();
-
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
+        b.iter_batched(
+            || {
+                let packet = Packet::event("/custom_nsp", "event", to_value(DATA).unwrap());
+                match packet.inner {
+                    PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
+                    _ => panic!("Wrong packet type"),
+                };
+                encode(packet)
+            },
+            decode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Decode packet ack on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String =
-            Packet::ack(black_box("/"), black_box(data.clone()), black_box(0)).into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
+        b.iter_batched(
+            || encode(Packet::ack("/", to_value(DATA).unwrap(), black_box(0))),
+            decode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Decode packet ack on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::ack(
-            black_box("/custom_nsp"),
-            black_box(data.clone()),
-            black_box(0),
+        b.iter_batched(
+            || encode(Packet::ack("/custom_nsp", to_value(DATA).unwrap(), 0)),
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.bench_function("Decode packet binary event (b64) on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::bin_event(
-            black_box("/"),
-            black_box("event"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
+        b.iter_batched(
+            || {
+                encode(Packet::bin_event(
+                    "/",
+                    "event",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY.clone()],
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.bench_function("Decode packet binary event (b64) on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::bin_event(
-            black_box("/custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
+        b.iter_batched(
+            || {
+                encode(Packet::bin_event(
+                    "/custom_nsp",
+                    "event",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY],
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.bench_function("Decode packet binary ack (b64) on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::bin_ack(
-            black_box("/"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-            black_box(0),
+        b.iter_batched(
+            || {
+                encode(Packet::bin_ack(
+                    "/",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY.clone()],
+                    0,
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.bench_function("Decode packet binary ack (b64) on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet: String = Packet::bin_ack(
-            black_box("/custom_nsp"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-            black_box(0),
+        b.iter_batched(
+            || {
+                encode(Packet::bin_ack(
+                    "/custom_nsp",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY.clone()],
+                    0,
+                ))
+            },
+            decode,
+            BatchSize::SmallInput,
         )
-        .into();
-        b.iter(|| Packet::try_from(packet.clone()).unwrap())
     });
 
     group.finish();
--- socketioxide/benches/packet_encode.rs
+++ socketioxide/benches/packet_encode.rs
@@ -1,150 +1,146 @@
 use bytes::Bytes;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use engineioxide::sid::Sid;
+use serde_json::to_value;
 use socketioxide::{
     packet::{Packet, PacketData},
+    parser::{CommonParser, Parse, TransportPayload},
     ProtocolVersion,
 };
+
+fn encode(packet: Packet<'_>) -> String {
+    match CommonParser::default().encode(black_box(packet)).0 {
+        TransportPayload::Str(d) => d.into(),
+        TransportPayload::Bytes(_) => panic!("testing only returns str"),
+    }
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("socketio_packet/encode");
+
     group.bench_function("Encode packet connect on /", |b| {
-        let packet = Packet::connect(black_box("/"), black_box(Sid::ZERO), ProtocolVersion::V5);
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::connect("/", Sid::ZERO, ProtocolVersion::V5),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
+
     group.bench_function("Encode packet connect on /custom_nsp", |b| {
-        let packet = Packet::connect(
-            black_box("/custom_nsp"),
-            black_box(Sid::ZERO),
-            ProtocolVersion::V5,
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::connect("/custom_nsp", Sid::ZERO, ProtocolVersion::V5),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     const DATA: &str = r#"{"_placeholder":true,"num":0}"#;
     const BINARY: Bytes = Bytes::from_static(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
 
     group.bench_function("Encode packet event on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::event(black_box("/"), black_box("event"), black_box(data.clone()));
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::event("/", "event", to_value(DATA).unwrap()),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet event on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::event(
-            black_box("custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::event("custom_nsp", "event", to_value(DATA).unwrap()),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet event with ack on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::event(black_box("/"), black_box("event"), black_box(data.clone()));
-        match packet.inner {
-            PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
-            _ => panic!("Wrong packet type"),
-        };
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || {
+                let packet = Packet::event("/", "event", to_value(DATA).unwrap());
+                if let PacketData::Event(_, _, mut ack) = packet.inner {
+                    let _ = ack.insert(black_box(0));
+                }
+                packet
+            },
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet event with ack on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::event(
-            black_box("/custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
-        );
-        match packet.inner {
-            PacketData::Event(_, _, mut ack) => ack.insert(black_box(0)),
-            _ => panic!("Wrong packet type"),
-        };
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || {
+                let packet = Packet::event("/custom_nsp", "event", to_value(DATA).unwrap());
+                if let PacketData::Event(_, _, mut ack) = packet.inner {
+                    let _ = ack.insert(black_box(0));
+                }
+                packet
+            },
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet ack on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::ack(black_box("/"), black_box(data.clone()), black_box(0));
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::ack("/", to_value(DATA).unwrap(), 0),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet ack on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::ack(
-            black_box("/custom_nsp"),
-            black_box(data.clone()),
-            black_box(0),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::ack("/custom_nsp", to_value(DATA).unwrap(), 0),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet binary event (b64) on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::bin_event(
-            black_box("/"),
-            black_box("event"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::bin_event("/", "event", to_value(DATA).unwrap(), vec![BINARY.clone()]),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet binary event (b64) on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::bin_event(
-            black_box("/custom_nsp"),
-            black_box("event"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || {
+                Packet::bin_event(
+                    "/custom_nsp",
+                    "event",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY.clone()],
+                )
+            },
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet binary ack (b64) on /", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::bin_ack(
-            black_box("/"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-            black_box(0),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || Packet::bin_ack("/", to_value(DATA).unwrap(), vec![BINARY.clone()], 0),
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.bench_function("Encode packet binary ack (b64) on /custom_nsp", |b| {
-        let data = serde_json::to_value(DATA).unwrap();
-        let packet = Packet::bin_ack(
-            black_box("/custom_nsp"),
-            black_box(data.clone()),
-            black_box(vec![BINARY.clone()]),
-            black_box(0),
-        );
-        b.iter(|| {
-            let _: String = packet.clone().into();
-        })
+        b.iter_batched(
+            || {
+                Packet::bin_ack(
+                    "/custom_nsp",
+                    to_value(DATA).unwrap(),
+                    vec![BINARY.clone()],
+                    0,
+                )
+            },
+            encode,
+            BatchSize::SmallInput,
+        )
     });
 
     group.finish();
