0a5dee1e68cc98c6e6fd0578b2c2bfc7f9e693c8
theodore <prevottheodore@gmail.com>
2025-01-01 20:41:08 +0100

    feat(redis): custom response serde

--- crates/socketioxide-redis/src/request.rs
+++ crates/socketioxide-redis/src/request.rs
@@ -2,7 +2,7 @@
 //! Custom serialization/deserialization to reduce the size of the messages.
 use std::collections::HashSet;
 
-use serde::{Deserialize, Serialize};
+use serde::{de::SeqAccess, Deserialize, Serialize};
 use socketioxide_core::{
     adapter::{BroadcastOptions, Room},
     packet::Packet,
@@ -145,14 +145,14 @@ impl<'de> Deserialize<'de> for RequestIn {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Response<D = ()> {
     pub uid: Uid,
     pub req_id: Sid,
     pub r#type: ResponseType<D>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, PartialEq)]
 pub enum ResponseType<D = ()> {
     BroadcastAck((Sid, Result<Value, D>)),
     BroadcastAckCount(u32),
@@ -167,6 +167,46 @@ impl<D> ResponseType<D> {
             Self::AllRooms(_) => 2,
             Self::FetchSockets(_) => 3,
         }
+    }
+}
+impl<D: Serialize> Serialize for ResponseType<D> {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self {
+            Self::BroadcastAck((sid, res)) => (0, (sid, res)).serialize(serializer),
+            Self::BroadcastAckCount(count) => (1, count).serialize(serializer),
+            Self::AllRooms(rooms) => (2, rooms).serialize(serializer),
+            Self::FetchSockets(sockets) => (3, sockets).serialize(serializer),
+        }
+    }
+}
+impl<'de, D: Deserialize<'de>> Deserialize<'de> for ResponseType<D> {
+    fn deserialize<DE: serde::Deserializer<'de>>(deserializer: DE) -> Result<Self, DE::Error> {
+        struct TupleVisitor<D>(std::marker::PhantomData<D>);
+        impl<'de, D: Deserialize<'de>> serde::de::Visitor<'de> for TupleVisitor<D> {
+            type Value = ResponseType<D>;
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(formatter, "a tuple of u8 and D")
+            }
+            fn visit_seq<A: SeqAccess<'de>>(self, mut seq: A) -> Result<Self::Value, A::Error> {
+                fn deser<'de, T: Deserialize<'de>, A: SeqAccess<'de>>(
+                    seq: &mut A,
+                ) -> Result<T, A::Error> {
+                    seq.next_element()?
+                        .ok_or_else(|| serde::de::Error::invalid_length(1, &""))
+                }
+
+                let el = match deser::<u8, _>(&mut seq)? {
+                    0 => ResponseType::BroadcastAck(deser(&mut seq)?),
+                    1 => ResponseType::BroadcastAckCount(deser(&mut seq)?),
+                    2 => ResponseType::AllRooms(deser(&mut seq)?),
+                    3 => ResponseType::FetchSockets(deser(&mut seq)?),
+                    _ => return Err(serde::de::Error::custom("invalid response type")),
+                };
+                Ok(el)
+            }
+        }
+
+        deserializer.deserialize_tuple(2, TupleVisitor::<D>(std::marker::PhantomData))
     }
 }
 impl<D> Response<D> {
@@ -186,6 +226,8 @@ impl<D> Response<D> {
 
 #[cfg(test)]
 mod tests {
+    use bytes::Bytes;
+
     use super::*;
 
     impl<'a> From<&'a RequestIn> for RequestOut<'a> {
@@ -257,5 +299,57 @@ mod tests {
         let opts = BroadcastOptions::new(Sid::new());
         let req = RequestOut::new(Uid::new(), RequestTypeOut::FetchSockets, &opts);
         assert_request_serde(req);
+    }
+
+    #[test]
+    fn response_serde_broadcast_ack() {
+        let res = Response {
+            uid: Uid::new(),
+            req_id: Sid::new(),
+            r#type: ResponseType::BroadcastAck((
+                Sid::new(),
+                Ok(Value::Bytes(Bytes::from_static(b"test"))),
+            )),
+        };
+        let serialized = rmp_serde::to_vec(&res).unwrap();
+        let deserialized: Response = rmp_serde::from_slice(&serialized).unwrap();
+        assert_eq!(res, deserialized);
+    }
+    #[test]
+    fn response_serde_broadcast_ack_count() {
+        let res = Response {
+            uid: Uid::new(),
+            req_id: Sid::new(),
+            r#type: ResponseType::BroadcastAckCount(42),
+        };
+        let serialized = rmp_serde::to_vec(&res).unwrap();
+        let deserialized: Response = rmp_serde::from_slice(&serialized).unwrap();
+        assert_eq!(res, deserialized);
+    }
+
+    #[test]
+    fn response_serde_all_rooms() {
+        let rooms = ["foo".into(), "bar".into()];
+        let res = Response {
+            uid: Uid::new(),
+            req_id: Sid::new(),
+            r#type: ResponseType::AllRooms(rooms.iter().cloned().collect()),
+        };
+        let serialized = rmp_serde::to_vec(&res).unwrap();
+        let deserialized: Response = rmp_serde::from_slice(&serialized).unwrap();
+        assert_eq!(res, deserialized);
+    }
+
+    #[test]
+    fn response_serde_fetch_sockets() {
+        let sockets = vec![Sid::new(), Sid::new()];
+        let res = Response {
+            uid: Uid::new(),
+            req_id: Sid::new(),
+            r#type: ResponseType::FetchSockets(sockets),
+        };
+        let serialized = rmp_serde::to_vec(&res).unwrap();
+        let deserialized: Response<Sid> = rmp_serde::from_slice(&serialized).unwrap();
+        assert_eq!(res, deserialized);
     }
 }
