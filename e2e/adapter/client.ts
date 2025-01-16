14ae20442140e3316cc015508409cc24137d8589
Théodore Prévot <prevottheodore@gmail.com>
2025-01-16 16:19:05 +0100

    test(e2e/adapter): rewrite test-suite and a lot more tests

--- e2e/adapter/client.ts
+++ e2e/adapter/client.ts
@@ -1,117 +1,250 @@
-import { spawn_servers, spawn_sockets, TEST, timeout } from "./fixture.ts";
+import { kill_servers, spawn_servers, spawn_sockets } from "./fixture.ts";
 import assert from "assert";
+import { describe, it, after, before, beforeEach, afterEach } from "node:test";
+import { Socket } from "socket.io-client";
 
 assert(!!process.env.CMD, "CMD env var must be set");
 
-// * Spawn 10 sockets on 3 servers
-// * Call a `broadcast` event on each socket
-// * Expect the socket to broadcast a message to all other sockets
-async function broadcast() {
-  const sockets = await spawn_sockets([3000, 3001, 3002], 10);
-  for (const socket of sockets) {
-    let msgs: string[] = [];
-    const prom = new Promise((resolve) => {
-      for (const socket of sockets) {
-        socket.once("broadcast", (data: string) => {
-          msgs.push(data);
-          if (msgs.length === sockets.length) resolve(null);
-        });
-      }
-    });
-    socket.emit("broadcast");
-    await timeout(prom);
-    assert.equal(Object.values(msgs).length, sockets.length);
-    for (const msg of msgs) {
-      assert.deepStrictEqual(msg, `hello from ${socket.id}`);
-    }
-  }
-  return sockets;
-}
+describe("adapter tests", { timeout: 10000 }, () => {
+  before(async (ctx) => {
+    const servers = await spawn_servers([3000, 3001, 3002]);
+    (ctx as any).servers = servers;
+  });
+  after(async (ctx) => {
+    kill_servers((ctx as any).servers);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    process.exit(0);
+  });
 
-async function broadcastWithAck() {
-  const sockets = await spawn_sockets([3000, 3001, 3002], 10);
-  const expected = sockets.map((s) => `ack from ${s.id}`).sort();
-  for (const socket of sockets) {
-    socket.on("broadcast_with_ack", (_data, ack) => {
-      ack(`ack from ${socket.id}`);
-    });
-  }
-  for (const socket of sockets) {
-    const res: string[] = await timeout(
-      socket.emitWithAck("broadcast_with_ack"),
-    );
-    assert.deepStrictEqual(res.sort(), expected);
-  }
-  return sockets;
-}
+  beforeEach(async (ctx) => {
+    const sockets = await spawn_sockets([3000, 3001, 3002], 10);
+    (ctx as any).sockets = sockets;
+  });
 
-async function disconnectSocket() {
-  const sockets = await spawn_sockets([3000, 3001, 3002], 10);
-  let cnt = 0;
-  const prom = new Promise((resolve) => {
+  afterEach(async (ctx) => {
+    const sockets: Socket[] = (ctx as any).sockets ?? [];
     for (const socket of sockets) {
-      socket.on("disconnect", () => {
-        cnt++;
-        if (cnt === sockets.length) resolve(null);
-      });
+      socket.disconnect();
     }
   });
-  sockets[0].emit("disconnect_socket");
-  await timeout(prom);
-  for (const socket of sockets) {
-    assert(!socket.connected);
-  }
-}
 
-async function rooms() {
-  const sockets = await spawn_sockets([3000, 3001, 3002], 10);
-  const expected = [
-    "room1",
-    "room2",
-    "room4",
-    "room5",
-    ...sockets.map((s) => s.id),
-  ].sort();
-  for (const socket of sockets) {
-    const rooms: string[] = await timeout(socket.emitWithAck("rooms"));
-    assert.deepStrictEqual(rooms.sort(), expected);
-  }
-  return sockets;
-}
+  describe("broadcast tests", () => {
+    it("should broadcast a packet sent from a socket to all other sockets", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      for (const socket of sockets) {
+        let msgs: string[] = [];
+        const prom = new Promise((resolve) => {
+          for (const socket of sockets) {
+            socket.once("broadcast", (data: string) => {
+              msgs.push(data);
+              if (msgs.length === sockets.length) resolve(null);
+            });
+          }
+        });
 
-// * Spawn 10 sockets on 3 servers
-// * Call a `fetch_sockets` event on each socket
-// * Get the list of sockets and compare it to the expected list
-async function fetchSockets() {
-  type SocketData = { id: string; ns: string };
-  const sockets = await spawn_sockets([3000, 3001, 3002], 10);
-  const expected = sockets
-    .map((socket) => ({
-      id: socket.id,
-      ns: "/",
-    }))
-    .sort((a, b) => a.id!.localeCompare(b.id!));
+        socket.emit("broadcast");
+        await prom;
+        assert.equal(Object.values(msgs).length, sockets.length);
+        for (const msg of msgs) {
+          assert.deepStrictEqual(msg, `hello from ${socket.id}`);
+        }
+      }
+    });
 
-  for (const socket of sockets) {
-    const data: SocketData[] = await timeout(
-      socket.emitWithAck("fetch_sockets"),
-    );
-    const sorted = data
-      ?.map((data) => ({ id: data.id, ns: data.ns }))
-      ?.sort((a, b) => a.id.localeCompare(b.id));
-    assert.deepStrictEqual(sorted, expected);
-  }
+    it("should broadcast a packet sent from a socket to all other sockets and get an ack from each socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      const expected = sockets.map((s) => `ack from ${s.id}`).sort();
+      for (const socket of sockets) {
+        socket.on("broadcast_with_ack", (_data, ack) => {
+          ack(`ack from ${socket.id}`);
+        });
+      }
+      for (const socket of sockets) {
+        const res: string[] = await socket.emitWithAck("broadcast_with_ack");
+        assert.deepStrictEqual(res.sort(), expected);
+      }
+    });
 
-  return sockets;
-}
+    it("should disconnect all sockets from a given one", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      let cnt = 0;
+      const prom = new Promise((resolve) => {
+        for (const socket of sockets) {
+          socket.on("disconnect", () => {
+            cnt++;
+            if (cnt === sockets.length) resolve(null);
+          });
+        }
+      });
+      sockets[0].emit("disconnect_socket");
+      await prom;
+      for (const socket of sockets) {
+        assert(!socket.connected);
+      }
+    });
 
-async function main() {
-  await spawn_servers([3000, 3001, 3002]);
-  await TEST(broadcast);
-  await TEST(broadcastWithAck);
-  await TEST(fetchSockets);
-  await TEST(disconnectSocket);
-  await TEST(rooms);
-  process.exit();
-}
-main();
+    it("should fetch all socket rooms from one socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      const expected = [
+        "room1",
+        "room2",
+        "room4",
+        "room5",
+        ...sockets.map((s) => s.id),
+      ].sort();
+      for (const socket of sockets) {
+        const rooms: string[] = await socket.emitWithAck("rooms");
+        assert.deepStrictEqual(rooms.sort(), expected);
+      }
+    });
+
+    it("should fetch all sockets from one socket", async (ctx) => {
+      type SocketData = { id: string; ns: string };
+      const sockets: Socket[] = (ctx as any).sockets;
+      const expected = sockets
+        .map((socket) => ({
+          id: socket.id,
+          ns: "/",
+        }))
+        .sort((a, b) => a.id!.localeCompare(b.id!));
+
+      for (const socket of sockets) {
+        const data: SocketData[] = await socket.emitWithAck("fetch_sockets");
+        const sorted = data
+          ?.map((data) => ({ id: data.id, ns: data.ns }))
+          ?.sort((a, b) => a.id.localeCompare(b.id));
+        assert.deepStrictEqual(sorted, expected);
+      }
+    });
+
+    it("should make all sockets join a room from one socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      const expected = [
+        "room1",
+        "room2",
+        "room4",
+        "room5",
+        ...sockets.map((s) => s.id),
+      ];
+      await sockets[0].emitWithAck("join_room");
+      const rooms: string[] = await sockets[0].emitWithAck("rooms");
+      assert.deepStrictEqual(rooms.sort(), [...expected, "room7"].sort());
+    });
+    it("should make all sockets leave a room from one socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      const expected = [
+        "room1",
+        "room2",
+        "room5",
+        ...sockets.map((s) => s.id),
+      ].sort();
+
+      await sockets[0].emitWithAck("leave_room");
+      const rooms: string[] = await sockets[0].emitWithAck("rooms");
+      assert.deepStrictEqual(rooms.sort(), expected);
+    });
+  });
+  describe("remote sockets actions", () => {
+    it("should emit from a remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+
+      for (let i = 0; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[(i + 1) % sockets.length];
+        const prom = new Promise((resolve) => socket_rcv.once("emit", resolve));
+        await socket_emit.emitWithAck("emit_from_remote_sock", socket_rcv.id);
+        assert.equal(await prom, `hello from ${socket_emit.id}`);
+      }
+    });
+
+    it("should emit with ack from a remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+
+      for (let i = 0; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[(i + 1) % sockets.length];
+        const prom = new Promise<[string, (ack: any) => void]>((resolve) =>
+          socket_rcv.once("emit", (msg, ack) => resolve([msg, ack])),
+        );
+        const ack = socket_emit.emitWithAck(
+          "emit_with_ack_from_remote_sock",
+          socket_rcv.id,
+        );
+        const [msg, ack_sender] = await prom;
+        assert.equal(msg, `hello from ${socket_emit.id}`);
+        ack_sender(`hi from ${socket_rcv.id}`);
+        assert.equal(await ack, `hi from ${socket_rcv.id}`);
+      }
+    });
+
+    it("should join a room from a remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+
+      for (let i = 0; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[(i + 1) % sockets.length];
+        await socket_emit.emitWithAck(
+          "join_room_from_remote_sock",
+          socket_rcv.id,
+        );
+        const rooms = await socket_emit.emitWithAck(
+          "get_rooms_remote_sock",
+          socket_rcv.id,
+        );
+        assert(rooms.includes(`hello from ${socket_emit.id}`));
+      }
+    });
+
+    it("should leave a room from a remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+
+      for (let i = 0; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[(i + 1) % sockets.length];
+        const rooms_before = await socket_emit.emitWithAck(
+          "get_rooms_remote_sock",
+          socket_rcv.id,
+        );
+        assert(rooms_before.includes("room4"));
+
+        await socket_emit.emitWithAck(
+          "leave_room_from_remote_sock",
+          socket_rcv.id,
+        );
+        const rooms = await socket_emit.emitWithAck(
+          "get_rooms_remote_sock",
+          socket_rcv.id,
+        );
+        assert(!rooms.includes(`room4`));
+      }
+    });
+
+    it("should disconnect a remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+
+      for (let i = 1; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[i - 1];
+        const prom = new Promise((resolve) =>
+          socket_rcv.once("disconnect", resolve),
+        );
+        await socket_emit.emitWithAck("disconnect_remote_sock", socket_rcv.id);
+        await prom;
+      }
+    });
+
+    it("should get rooms from remote socket", async (ctx) => {
+      const sockets: Socket[] = (ctx as any).sockets;
+      const ROOMS = ["room1", "room2", "room4", "room5"];
+      for (let i = 0; i < sockets.length; i++) {
+        const socket_emit = sockets[i];
+        const socket_rcv = sockets[(i + 1) % sockets.length];
+        const rooms = await socket_emit.emitWithAck(
+          "get_rooms_remote_sock",
+          socket_rcv.id,
+        );
+        assert.deepEqual(rooms.sort(), [...ROOMS, socket_rcv.id].sort());
+      }
+    });
+  });
+});
--- e2e/adapter/fixture.ts
+++ e2e/adapter/fixture.ts
@@ -1,58 +1,34 @@
 import io, { Socket } from "socket.io-client";
 import msgpackParser from "socket.io-msgpack-parser";
-import { ChildProcess, exec, spawn } from "child_process";
+import { ChildProcess, spawn } from "child_process";
 import assert from "assert";
 import { open } from "fs/promises";
 
-export async function timeout_recv<T>(
-  fn: (resolve: (value: T) => void) => any,
-  duration = 500,
-) {
-  return new Promise<T>((resolve, reject) => {
-    fn(resolve);
-    setTimeout(() => reject("timeout"), duration);
-  });
-}
-export async function timeout<T>(
-  promise: Promise<T>,
-  duration = 500,
-): Promise<T> {
-  return new Promise<T>((resolve, reject) => {
-    setTimeout(() => reject("timeout"), duration);
-    promise.then(resolve, reject);
-  });
+export async function kill_servers(servers: ChildProcess[]) {
+  for (const server of servers) {
+    server.kill();
+  }
 }
 
 export async function spawn_servers(ports: number[]) {
   const args = process.env.CMD!.split(" ");
-  const bin = args.shift();
-  const servers: [ChildProcess, number][] = [];
-  const logs: Record<number, string> = {};
+  const bin = args.shift()!;
+  const servers: ChildProcess[] = [];
   for (const port of ports) {
-    exec(`kill $(lsof -t -i:${port})`);
-    console.log("spawning server on port", port);
     const file = (await open(`${port}.log`, "w")).createWriteStream();
     console.log(`EXEC PORT=${port} ${bin} ${args.join(" ")}`);
     const server = spawn(bin, args, {
-      shell: true,
+      detached: false,
+      timeout: 10000,
       env: {
         ...process.env,
         PORT: port.toString(),
       },
     });
-    logs[server.pid] = "";
     server.stdout.pipe(file);
     server.stderr.pipe(file);
+    servers.push(server);
   }
-  process.on("exit", () => {
-    for (const [server, port] of servers) {
-      console.log("killing", server.pid);
-      server.kill();
-      exec(`kill $(lsof -t -i:${port})`);
-    }
-  });
-  process.on("SIGINT", () => process.exit()); // catch ctrl-c
-  process.on("SIGTERM", () => process.exit()); // catch kill
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return servers;
 }
@@ -60,29 +36,17 @@ export async function spawn_servers(ports: number[]) {
 // Spawn a number of distributed sockets on a list of ports
 export async function spawn_sockets(ports: number[], len: number) {
   let sockets: Socket[] = [];
-  const parser = process.env.CMD.includes("msgpack") ? msgpackParser : null;
+  const parser = process.env.CMD?.includes("msgpack") ? msgpackParser : null;
   for (let i = 0; i < len; i++) {
     const socket = io(`http://localhost:${ports[i % ports.length]}`, {
       parser,
     });
     assert(
-      await timeout_recv((resolve) =>
-        socket.on("connect", () => resolve(true)),
+      await new Promise((resolve) =>
+        socket.once("connect", () => resolve(true)),
       ),
     );
     sockets.push(socket);
   }
   return sockets;
-}
-export async function TEST(
-  fn: () => Promise<void | Socket[]>,
-): Promise<void | Socket[]> {
-  console.log(`RUN ${fn.name}`);
-  const sockets = await fn();
-  if (sockets) {
-    for (const socket of sockets) {
-      socket.disconnect();
-    }
-  }
-  console.log(`OK ${fn.name}`);
 }
