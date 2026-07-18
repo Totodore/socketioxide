d4c66185924a3eaf250a2f1703a8b1d63893c6d1
tprevot <tprevot@centreon.com>
2026-07-18 18:46:46 +0200

    test: engine poll

--- crates/engineioxide-client/tests/fixture.rs
+++ crates/engineioxide-client/tests/fixture.rs
@@ -39,7 +39,10 @@ fn init_tracing() {
 
 pub fn service_with_config(
     config: EngineIoConfig,
-) -> (TestingFlavor<EngineIoService<EchoHandler>>, mpsc::UnboundedReceiver<Event>) {
+) -> (
+    TestingFlavor<EngineIoService<EchoHandler>>,
+    mpsc::UnboundedReceiver<Event>,
+) {
     init_tracing();
     let (handler, rx) = EchoHandler::new();
     let svc = EngineIoService::with_config(Arc::new(handler), config);
@@ -47,7 +50,10 @@ pub fn service_with_config(
 }
 
 #[allow(unused)]
-pub fn service() -> (TestingFlavor<EngineIoService<EchoHandler>>, mpsc::UnboundedReceiver<Event>) {
+pub fn service() -> (
+    TestingFlavor<EngineIoService<EchoHandler>>,
+    mpsc::UnboundedReceiver<Event>,
+) {
     service_with_config(EngineIoConfig::default())
 }
 
