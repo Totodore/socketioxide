d231af257848d5ca6234eda9b20b10c1a821abfa
totodore <prevottheodore@gmail.com>
2026-09-01 22:34:06 +0200

    ci: check features

--- crates/engineioxide-client/src/flavors/hyper.rs
+++ crates/engineioxide-client/src/flavors/hyper.rs
@@ -10,7 +10,15 @@ use hyper_util::client::legacy::{
 };
 use tower_service::Service;
 
-use crate::flavors::noop_impl::NoopWebSocket;
+use std::{
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use futures_core::Stream;
+use futures_util::Sink;
+
+use crate::transport::ws::{WebSocket, WsMessage};
 
 #[derive(Debug, Clone)]
 pub struct HyperFlavor {
@@ -63,5 +71,39 @@ impl Service<http::Request<()>> for HyperFlavor {
 
     fn call(&mut self, _: http::Request<()>) -> Self::Future {
         std::future::ready(Ok(NoopWebSocket))
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct NoopWebSocket;
+impl WebSocket for NoopWebSocket {
+    type Error = Infallible;
+}
+
+impl Stream for NoopWebSocket {
+    type Item = Result<WsMessage, Infallible>;
+
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        Poll::Ready(None)
+    }
+}
+
+impl Sink<WsMessage> for NoopWebSocket {
+    type Error = Infallible;
+
+    fn poll_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn start_send(self: Pin<&mut Self>, _item: WsMessage) -> Result<(), Self::Error> {
+        Ok(())
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
     }
 }
